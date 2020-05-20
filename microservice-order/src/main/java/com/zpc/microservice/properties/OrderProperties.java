1bbb1940e60565b2de3b216e5a114da7033567b9
shanxianyue <279149639@qq.com>
2020-05-20 14:26:12 +0800

    改造-消除硬编码

--- microservice-order/src/main/java/com/zpc/microservice/properties/OrderProperties.java
+++ microservice-order/src/main/java/com/zpc/microservice/properties/OrderProperties.java
@@ -1,4 +1,19 @@
 package com.zpc.microservice.properties;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix="myspcloud")
 public class OrderProperties {
+
+    private ItemProperties item = new ItemProperties();
+
+    public ItemProperties getItem() {
+        return item;
+    }
+
+    public void setItem(ItemProperties item) {
+        this.item = item;
+    }
 }
--- microservice-order/src/main/java/com/zpc/microservice/service/ItemService.java
+++ microservice-order/src/main/java/com/zpc/microservice/service/ItemService.java
@@ -1,6 +1,7 @@
 package com.zpc.microservice.service;
 
 import com.zpc.microservice.entity.Item;
+import com.zpc.microservice.properties.OrderProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -14,9 +15,11 @@ public class ItemService {
     // Spring框架对RESTful方式的http请求做了封装，来简化操作
     @Autowired
     private RestTemplate restTemplate;
+    @Autowired
+    private OrderProperties orderProperties;
 
     public Item queryItemById(Long id) {
-        return this.restTemplate.getForObject("http://127.0.0.1:8081/item/"
+        return this.restTemplate.getForObject(orderProperties.getItem().getUrl()
                 + id, Item.class);
     }
 }
